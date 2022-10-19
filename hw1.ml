48a5ac3b0a56ddb105086ad0c9e7c73e49b79e30
Sylvia <sylvialee919@gmail.com>
2022-10-19 13:58:26 -0400

    Add files via upload

--- hw1.ml
+++ hw1.ml
@@ -1,13 +1,12 @@
 (**HW 1 Due 9/25/2022*)
 
 (*Question 1*)
-let rec pow x n= if n = 0 then 1 else x * pow x (n-1);;
+let rec pow x n = 
+          if n = 0 then 1 else x * pow x (n-1);;
 
-print_int (pow 2 3);;
-let rec float_pow x n= if n=0 then 1.0 else x *. float_pow x (n-1);;
+let rec float_pow x n = 
+          if n = 0 then 1.0 else x *. float_pow x (n-1);;
 
-print_string("\n");;
-print_float (float_pow 3.5 0);;
 
 (*Question 2*)
 let rec compress lst = 
@@ -15,18 +14,21 @@ let rec compress lst =
   | first:: (second:: _ as remain) -> 
     if first=second then compress remain 
     else first:: compress remain
-  | origlist ->origlist;;
+  | origlist -> origlist;;
+
 
 (*Question 3*)  
-let rec remove_if lst f=
+let rec remove_if lst f =
     match lst with
+    | [] -> []
     | first::remain -> 
       if f(first) then remove_if remain f 
-      else first:: remove_if remain f
-    | [] -> [];;
+      else first:: remove_if remain f;;
+
+
 
 (*Question 4*)
-let rec slice lst i j= 
+let rec slice lst i j = 
       match lst with
       | [] -> []
       | head::remain -> 
@@ -37,13 +39,111 @@ let rec slice lst i j=
             if i >= 1 then r 
             else head::r;;
 
+      (*TRACING
+      ["a";"b";"c";"d";"e";"f";"g";"h"] 2 6;;
+      ["b";"c";"d";"e";"f";"g";"h"] 1 5;;
+      ["c";"d";"e";"f";"g";"h"] 0 4;;
+      ["d";"e";"f";"g";"h"] -1 3;;
+      ["e";"f";"g";"h"] -2 2;;
+      ["f";"g";"h"] 2 6;; -> r becomes [] so ["f"] and go back*)
+
+
+
 (*Question 5*)
+let rec equivs f lst= 
+    match lst with
+    | [] -> []
+    | head::tail ->
+        let rec test lst_ bool=
+        match lst_ with 
+        | [] -> []
+        | h::t ->
+            if (f h head) == bool then h::test t bool
+            else test t bool
+          in test lst true::equivs f (test lst false);;
+            
+
+
+(*Question 6*)
+let rec range start stop =
+  if start=stop then []
+  else start::(range (start+1) stop);;
+
+
+let prime num =   
+  let rec isPrime lst =
+    match lst with
+    | [] -> true
+    | head::tail -> 
+        if num mod head = 0 then false
+        else isPrime tail 
+    in isPrime (range 2 num);;
+
+
+let primeLst num = 
+  let x= range 3 num in 
+  let rec random lst = 
+    match lst with 
+    | [] -> []
+    | head::tail -> 
+        if prime head = true then head::random tail
+        else random tail
+      in random x;;
+
+let goldbachpair num = 
+    let x = primeLst num in 
+    let rec checkLst lst1 lst2 = 
+    match lst1,lst2 with 
+    | [], _ -> (0,0)
+    | head::tail, head2::tail2 ->
+        if head + head2 = num then (head , head2) 
+        else checkLst lst1 tail2
+    | head::tail, [] -> 
+        checkLst tail lst2
+      in checkLst x x;;
+      
+
+(*Question 7*)
+let rec equiv_on f g lst =
+  match lst with
+  | [] -> true
+  | head::remain -> 
+  if f(head)=g(head) then equiv_on f g remain
+  else false;;
+
+
+(*Question 8*)
+let rec pairwisefilter cmp lst =
+  match lst with
+  | [] -> []
+  | first::[] -> [first]
+  | first::second::remain -> let remain2= pairwisefilter cmp remain in 
+       cmp first second::remain2;;
+            
+
+(*Question 9*)
+let rec polynomial lst = 
+    fun x -> match lst with
+             | [] -> 0
+             | (a,b)::remain -> let restlst = polynomial remain in
+                                          let num = (pow x b) in (a* num) + restlst x;;
 
 
 
+(*Question 10*)
+let rec map f lst = 
+  match lst with
+  | [] -> []
+  | head::remain -> f(head)::map f remain;;
 
-    
- 
+let rec append lst1 lst2 =
+  match lst1 with
+  | [] -> lst2
+  | head::remain -> head :: append remain lst2;;
+              
 
-
-
+ let rec powerset lst = 
+  match lst with
+  | [] -> [[]]
+  | head::remain -> let mapRest = powerset remain in 
+    append mapRest (map (fun x -> head::x) mapRest);;
